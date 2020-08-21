f737d0679149e6fe458135249fab22e4a852643c
CyrilBrujah <kriogen0501@gmail.com>
2020-08-21 19:30:31 +0300

    Change View classes to decorated functions

--- reader/urls.py
+++ reader/urls.py
@@ -1,19 +1,17 @@
 from django.urls import path
 
-from .views import GetCodeView, GetTokenView, ProfileView
+from .views import GetCodeView, GetTokenView, ProfileView, get_code, token
 from rest_framework import routers
-
-
 
 app_name = "reader"
 
-router = routers.DefaultRouter()
-router.register(r'profiles', ProfileView, basename='profiles')
-
 
 urlpatterns = [
-    path('get_code/', GetCodeView.as_view()),
-    path('token/', GetTokenView.as_view()),
+    path('get_code/', get_code),
+    path('token', token),
 ]
+
+router = routers.DefaultRouter()
+router.register(r'profiles', ProfileView, basename='profiles')
 
 urlpatterns += router.urls
--- reader/views.py
+++ reader/views.py
@@ -6,6 +6,7 @@ from rest_framework import generics
 from rest_framework.mixins import ListModelMixin
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect, get_object_or_404
 
@@ -17,16 +18,37 @@ from .serializers import ProfileSerializer
 import requests
 
 
-class CsrfExemptSessionAuthentication(SessionAuthentication):
-    def enforce_csrf(self, request):
-        return
+@api_view(['GET'])
+def get_code(request, format=None):
+    response = requests.get("https://oauth.vk.com/authorize/",
+                            #headers={'Content-Type': 'application/json'},
+                            params={'client_id': 7572251, 'redirect_uri': 'http://127.0.0.1:8000/api/token', 'display': 'page', 'response_type': 'code', 'v': '5.122'})
+    return HttpResponseRedirect(response.url)
+
+
+@api_view(["GET"])
+def token(request, format=None):
+    code = request.query_params['code']
+    response = requests.get("https://oauth.vk.com/access_token",
+                            params={'client_id': 7572251, 'client_secret': '5IwMHn55CcxuhnDt8dZj', 'redirect_uri': 'http://127.0.0.1:8000/api/token', 'code': code})
+    access_token = response.json()['access_token']
+    user_id = response.json()['user_id']
+    response = requests.get("https://api.vk.com/method/users.get",
+                            params={'user_id': user_id, 'access_token': access_token, 'v': '5.122'})
+    result = response.json()['response']
+    result = result[0]
+    vk_id = result['id']
+    first_name = result['first_name']
+    last_name = result['last_name']
+    Profile.objects.create(vk_id=vk_id, first_name=first_name,
+                           last_name=last_name, access_token=access_token)
+    return Response(response.json())
 
 
 class GetCodeView(APIView):
     def get(self, request):
         response = requests.get("https://oauth.vk.com/authorize/",
-                                #headers={'Content-Type': 'application/json'},
-                                params={'client_id': 7572251, 'redirect_uri': 'http://127.0.0.1:8000/api/token', 'display': 'page', 'response_type': 'code', 'v': '5.122', 'scope': 'friends'})
+                                params={'client_id': 7572251, 'redirect_uri': 'http://127.0.0.1:8000/api/token', 'display': 'page', 'response_type': 'code', 'v': '5.122'})
         return HttpResponseRedirect(response.url)
 
 
